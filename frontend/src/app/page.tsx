8fe418d56dfec9bd9bcab13294d8d31c5c725cd7
abbieduan <abbiehduan@gmail.com>
2025-06-26 14:01:42 +0100

    added upload file component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,17 @@
 export default function Home() {
 	return (
 		<main className="p-8">
-			<div className="card bg-amber-600 text-yellow-200">
-				<div className="card-body">
-					<h2 className="card-title text-xl font-semibold">Welcome</h2>
-					<p className="mt-2">
-						This app helps small businesses get AI-powered financial insights
-						using WatsonX and Granite 3.3.
-					</p>
+			<div className="hero bg-orange-400 min-h-screen text-yellow-200">
+				<div className="hero-content text-center">
+					<div className="max-w-md">
+						<h1 className="text-5xl font-bold">Hello there</h1>
+						<p className="py-6">
+							Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+							excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+							et a id nisi.
+						</p>
+						<button className="btn">Get Started</button>
+					</div>
 				</div>
 			</div>
 		</main>
--- frontend/src/app/upload/page.tsx
+++ frontend/src/app/upload/page.tsx
@@ -2,10 +2,7 @@ export default function UploadPage() {
 	return (
 		<main>
 			<h2 className="text-xl font-semibold">Upload Report</h2>
-			<p className="mt-2">
-				Upload your financial document to receive insights.
-			</p>
-			{/* Form or drag-and-drop goes here later */}
+			<input type="file" className="file-input" />
 		</main>
 	);
 }
